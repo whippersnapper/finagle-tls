e17f4387a431002ec4c2cd327c55e487c7d50967
Daniel Cox <daniel.cox@deciphertechstudios.com>
2015-09-02 13:36:54 -0400

    requiring client cert

--- src/main/scala/com/twitter/finagle/example/tls/TLSServer.scala
+++ src/main/scala/com/twitter/finagle/example/tls/TLSServer.scala
@@ -1,9 +1,11 @@
 package com.twitter.finagle.example.tls
 
+import java.security.KeyStore
 import java.security.cert.X509Certificate
+import javax.net.ssl.{SSLEngine, SSLContext, TrustManagerFactory, KeyManagerFactory}
 
 import com.twitter.finagle.Thrift
-import com.twitter.finagle.ssl.Ssl
+import com.twitter.finagle.ssl.{Engine, Ssl}
 import com.twitter.finagle.transport.Transport
 import com.twitter.util.{Await, Future}
 
@@ -14,7 +16,11 @@ object TLSServer {
   def main(args: Array[String]) {
 
     val server = Thrift.server
-      .configured(Transport.TLSServerEngine(Some(() => Ssl.server(SslFile.serverCrt, SslFile.serverKey, null, null, null))))
+      .configured(Transport.TLSServerEngine(Some(() => {
+        val engine: SSLEngine = createSslContext.createSSLEngine()
+        engine.setNeedClientAuth(true)
+        new Engine(engine)
+      })))
       .serveIface(":8080", new DogBeauty.FutureIface {
       override def isBreedBeautiful(request: BeautifulDogRequest): Future[BeautifulDogResponse] = {
 
@@ -28,6 +34,19 @@ object TLSServer {
       }
     })
     Await.ready(server)
+  }
+  private lazy val createSslContext = {
+    val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+    val trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+    val keyStore = KeyStore.getInstance(KeyStore.getDefaultType)
+    val trustKeyStore = KeyStore.getInstance("JKS")
+    keyStore.load(SslFile.keyStoreStream, SslFile.clientPassword.toCharArray)
+    trustKeyStore.load(SslFile.trustKeyStoreStream, SslFile.clientPassword.toCharArray)
+    keyManagerFactory.init(keyStore, SslFile.clientPassword.toCharArray)
+    trustManagerFactory.init(trustKeyStore)
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, null)
+    sslContext
   }
 }
 
